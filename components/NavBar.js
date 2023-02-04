e124bb1688fe79b22ff36b14d7d465245dae660e
verobeach7 <verobeach7@gmail.com>
2023-02-04 16:04:04 +0900

    NextJS: Styled JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css"; // css module: normal css를 사용할 수 있게 해줌
 
 export default function NavBar() {
   const router = useRouter();
   console.log(router);
   return (
     <nav>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        href="/"
-      >
-        Home
+      <Link style={{ textDecoration: "none" }} href="/">
+        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
       </Link>
-      <Link
-        // .join은 배열 안의 item을 하나의 문자열로 바꾸는 method
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-        href="/about"
-      >
-        About
+      <Link style={{ textDecoration: "none" }} href="/about">
+        <span className={router.pathname === "/about" ? "active" : ""}>
+          About
+        </span>
       </Link>
+      {/* style은 html 고유 tag */}
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,13 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar></NavBar>
-      <h1>Hi</h1>
+      <NavBar />
+      <h1 className="active">Hi</h1>
+      <style jsx>{`
+        a {
+          color: white;
+        }
+      `}</style>
     </div>
   );
 }
