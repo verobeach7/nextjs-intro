e3fcf58a3bd8164f07cd9ccd8ba928d8e9677543
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-02-04 20:44:58 +0900

    NextJS: Redirect & Rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*", // 이전 주소로 누군가 접근하면
+        destination: "/new-sexy-blog/:path*", // 자동으로 새로운 주소로 바꿔줌
+        permanent: false,
+      },
+      // 여러개의 redirect가 필요하면 객체{}를 계속해서 생성해주면 됨
+    ];
+  },
+  // rewrites는 redirect를 시키지만 사용자에게 변경된 url을 보여주지 않음!!!
+  // 개발자도구 Network 탭에서도 숨겨짐
+  // API_KEY 등 사용자에게 보여주지 않아야 하는 정보가 있을 때 필수적으로 사용!!!
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,45 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-const API_KEY = "f0c6d61ec6f0982e9ba19ec4ec010a8d";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })(); // 익명함수를 바로 실행하기 위해서 () 붙여주는 것!!!
   }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
