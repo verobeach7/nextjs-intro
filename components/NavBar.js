4dc0134ad94ebfe611c1388de4012c094bcd6141
verobeach7 <verobeach7@gmail.com>
2023-02-04 15:05:00 +0900

    NextJS: CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
+import styles from "./NavBar.module.css"; // css module: normal css를 사용할 수 있게 해줌
 
 export default function NavBar() {
   const router = useRouter();
   console.log(router);
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link
-        style={{ color: router.pathname === "/" ? "red" : "blue" }}
+        className={`${styles.link} ${
+          router.pathname === "/" ? styles.active : ""
+        }`}
         href="/"
       >
         Home
       </Link>
       <Link
-        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
-        className="hello"
+        // .join은 배열 안의 item을 하나의 문자열로 바꾸는 method
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}
         href="/about"
       >
         About
